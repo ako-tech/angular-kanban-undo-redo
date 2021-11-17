45cc3835eb3dbcc0f40b9ab739dc2f3d99b09c24
akotech <akotech20@gmail.com>
2021-11-17 21:42:50 +0100

    add CommandManager main logic

--- src/app/command-manager/command-manager.service.ts
+++ src/app/command-manager/command-manager.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Command } from '.';
 
 @Injectable()
-export class CommandManagerService {}
+export class CommandManagerService {
+  private doneCommandsStack: Command[] = [];
+  private undoneCommandsStack: Command[] = [];
+
+  execute(command: Command): void {
+    command.execute();
+
+    this.doneCommandsStack.push(command);
+
+    this.resetUndoneStack();
+  }
+
+  undo(): void {
+    if (this.doneCommandsStack.length === 0) {
+      return;
+    }
+
+    const lastDoneCommand = this.doneCommandsStack.pop()!;
+    lastDoneCommand.undo();
+
+    this.undoneCommandsStack.push(lastDoneCommand);
+  }
+
+  redo(): void {
+    if (this.undoneCommandsStack.length === 0) {
+      return;
+    }
+
+    const lastUndoneCommand = this.undoneCommandsStack.pop()!;
+    lastUndoneCommand.execute();
+
+    this.doneCommandsStack.push(lastUndoneCommand);
+  }
+
+  private resetUndoneStack(): void {
+    this.undoneCommandsStack = [];
+  }
+}
